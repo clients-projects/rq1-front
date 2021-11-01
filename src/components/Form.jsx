a3cfced0e0c548c343a85e9fee945db976944eb6
Igboanugwo Collins <munisco12@gmail.com>
2021-11-01 22:02:04 +0100

    styling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -40,7 +40,7 @@ const Form = (props) => {
     return (
         <form
             className='grid w-full place-content-stretch bg-white '
-            style={{ padding: '32px 24px' }}
+            style={{ padding: '30px 15px' }}
             onSubmit={handleSubmit}
         >
             <h2 className='font-medium text-center text-[#1c124d] mb-4 text-2xl'>
