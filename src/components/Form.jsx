04e7794517989abee40c4ca062a6fd7ad46d0c51
Igboanugwo Collins <munisco12@gmail.com>
2021-11-01 19:04:14 +0100

    working on styling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -46,7 +46,7 @@ const Form = (props) => {
             <input
                 type='email'
                 id='email'
-                className='text-black outline-none text-lg p-1 rounded-md justify-self-stretch placeholder-gray-[#b2b7be]'
+                className='text-black outline-none text-lg p-1 rounded-md justify-self-stretch placeholder-[#b2b7be]'
                 required
                 value={phrase}
                 placeholder='Email address'
