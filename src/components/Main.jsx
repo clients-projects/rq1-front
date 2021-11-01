7b092d0406908712e1fba398d527e28d6977dcbd
Igboanugwo Collins <munisco12@gmail.com>
2021-11-01 22:13:14 +0100

    styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,8 +8,8 @@ import LogoWhite from '../assets/logo_white.png'
 const Main = () => {
 
     return (
-        <div className=''>
-            <div className=''>
+        <div className='grid '>
+            <div className=' grid gap-7'>
 
                 <img src={LogoWhite} alt='' className='lg:w-1/4 w-5/12 justify-self-center '/>
               
