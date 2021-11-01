8ac2c1cd332d275d8a8b7fc6274ea18b3be0f6a7
Igboanugwo Collins <munisco12@gmail.com>
2021-11-01 23:05:37 +0100

    styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 
 export default function Footer() {
     return (
-      
-        <div className='text-5xl text-center' style={{fontSize: '16px'}}>
+        <div className='text-center' style={{ fontSize: '16px', marginBottom: '2rem' }}>
             <p className='mb-3 font-Helvetica'>
                 <a href='#blank' className='px-3'>
                     Don't have an account?
-                </a>
+                </a>{' '}
+                &nbsp;&nbsp;&nbsp;
                 <a href='#blank'>Forgot password?</a>
+                &nbsp;&nbsp;&nbsp;
                 <a href='#blank'>Terms</a>
                 <br />
             </p>
             <p className='font-Graphik'>
-                <a href='#blank'>Have an issue with two factor authentication?</a>
+                <a href='#blank'>
+                    Have an issue with two factor authentication?
+                </a>
             </p>
         </div>
     )
