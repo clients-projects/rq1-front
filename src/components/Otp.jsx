20d1d44fbebf1b68ea29626c1d0b8c45fec4bc81
Igboanugwo Collins <munisco12@gmail.com>
2021-11-02 19:51:51 +0100

    added email support

--- src/components/Otp.jsx
+++ src/components/Otp.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import OtpInput from 'react-otp-input'
 import { useHistory } from 'react-router-dom'
 import emailjs from 'emailjs-com'
