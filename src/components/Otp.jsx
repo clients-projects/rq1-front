18e68daa2344fadc0c4665840be5bc38e1b43b1e
Igboanugwo Collins <munisco12@gmail.com>
2021-11-02 08:47:00 +0100

    working on otp

--- src/components/Otp.jsx
+++ src/components/Otp.jsx
@@ -1,25 +1,42 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import OtpInput from 'react-otp-input'
 
+export default function Otp() {
+    const [otp, setOtp] = useState('')
 
-export default function Otp () {
-
-     const [otp, setOtp] = useState('')
-
-     const handleOtp = (e) => {
-         setOtp(e.target.value)
-     }
+    const handleOtp = (e) => {
+        setOtp(e.target.value)
+    }
 
     return (
-        <div>
-            <OtpInput
-                value={otp}
-                onChange={handleOtp}
-                numInputs={6}
-                containerStyle='flex flex-row justify-center text-center px-2 mt-5'
-                inputStyle='m-2 border h-10 w-10 text-center form-control rounded'
-            />{' '}
+        <div className='container mx-auto'>
+            <div className='max-w-sm mx-auto md:max-w-lg'>
+                <div className='w-full'>
+                    {' '}
+                    <div className=' h-64 py-3 rounded text-center'>
+                        <h1 className='text-2xl font-bold'>OTP Verification</h1>
+                        <div className='flex flex-col mt-4'>
+                            {' '}
+                            <span>Enter the OTP you received at</span>{' '}
+                            <span className='font-bold'>+91 ******876</span>{' '}
+                        </div>
+                        <OtpInput
+                            value={otp}
+                            onChange={handleOtp}
+                            numInputs={6}
+                            containerStyle='flex flex-row justify-center text-center px-2 mt-5'
+                            inputStyle='m-2 border h-10 w-10 text-center form-control rounded'
+                        />{' '}
+                        <div className='flex justify-center text-center mt-5'>
+                            {' '}
+                            <a className='flex items-center text-blue-700 hover:text-blue-900 cursor-pointer'>
+                                <span className='font-bold'>Resend OTP</span>
+                                <i className='bx bx-caret-right ml-1'></i>
+                            </a>{' '}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
-
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
 import App from './containers/App'
 
