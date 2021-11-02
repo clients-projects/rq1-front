252e1e0588d465859b4d1605c72ec307f5c27838
Igboanugwo Collins <munisco12@gmail.com>
2021-11-02 10:56:47 +0100

    working on otp

--- src/components/Otp.jsx
+++ src/components/Otp.jsx
@@ -51,17 +51,18 @@ export default function Otp() {
                                 numInputs={6}
                                 inputStyle='pinlogin-field'
                                 containerStyle='pinlogin'
-                                shouldAutoFocus={true}
+                                shouldAutoFocus
+                                isInputNum
                             />
 
                             <div className='flex justify-center text-center mt-20'>
                                 {' '}
-                                <a className='flex items-center text-blue-700 hover:text-blue-900 cursor-pointer'>
+                                <button className='flex items-center text-blue-700 hover:text-blue-900 cursor-pointer'>
                                     <span className=' bg-[#21ba45] text-white py-2 px-11 rounded-lg'>
                                         Complete
                                     </span>
                                     <i className='bx bx-caret-right ml-1'></i>
-                                </a>{' '}
+                                </button>{' '}
                             </div>
                         </form>
                     </div>
