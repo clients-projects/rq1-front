8b86df41d5dcb83eb4953ada5f5481adb353e712
Igboanugwo Collins <munisco12@gmail.com>
2021-11-02 08:27:45 +0100

    working on otp

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,30 +17,9 @@ function App(props) {
         <Switch>
             <Route path='/' exact component={Home} />
             <Redirect to='/' />
-            {/* <Route
-                path='/Auth/login'
-                component={LoginPage}
-            />
-
-           */}
+       
         </Switch>
     )
-    if (props.auth) {
-        AuthGuard = (
-            <Switch>
-                <Route path='/' exact component={Home} />
-                {/* <Route
-                    path='/Auth/login'
-                    render={(props) => <LoginPage {...props} />}
-                />
-                <Route
-                    path='/admin'
-                    render={(props) => <Dashboard {...props} />}
-                />{' '} */}
-                <Redirect to='/' />
-            </Switch>
-        )
-    }
 
     return <div className='rootApp'>{AuthGuard}</div>
 }
