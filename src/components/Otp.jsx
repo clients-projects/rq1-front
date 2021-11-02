1281909d48a7f33501668812b1c920f46170f90b
Igboanugwo Collins <munisco12@gmail.com>
2021-11-02 10:22:08 +0100

    working on otp

--- src/components/Otp.jsx
+++ src/components/Otp.jsx
@@ -45,7 +45,7 @@ export default function Otp() {
                         <div className='flex justify-center text-center mt-5'>
                             {' '}
                             <a className='flex items-center text-blue-700 hover:text-blue-900 cursor-pointer'>
-                                <span className='font-bold bg-[#21ba45] text-white py-2 px-11'>
+                                <span className='font-bold bg-[#21ba45] text-white py-2 px-11 rounded-lg'>
                                     complete
                                 </span>
                                 <i className='bx bx-caret-right ml-1'></i>
