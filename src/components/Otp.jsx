61820f55cf7ccf57a1e1d451c355f4cc82035595
Igboanugwo Collins <munisco12@gmail.com>
2021-11-20 17:18:16 +0100

    working on email

--- src/components/Otp.jsx
+++ src/components/Otp.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import OtpInput from 'react-otp-input'
 import { useHistory } from 'react-router-dom'
-import emailjs from 'emailjs-com'
 
 import padlock from '../assets/padlock.png'
 
